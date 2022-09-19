4e312788247f3d119af2346f38f56d7115383995
Christoph <christophwelling@uchicago.edu>
2022-09-19 12:55:56 -0500

    Includes ROOT plotting for shower profile

--- shower_profile/main.cpp
+++ shower_profile/main.cpp
@@ -2,15 +2,18 @@
 #include <array>
 #include <iostream>
 #include "shower_creator.h"
+#include "TCanvas.h"
+#include "TGraph.h"
 int main () {
-	std::array<float, 3> pos = {1.0, 2.0, 3.0};
+	std::array<float, 3> pos = {20, 20, 20};
+	showers::ShowerCreator shower_creator("/home/welling/RadioNeutrino/scripts/Eisvogel/shower_profile/shower_file");
+	double pi = acos(-1);
 
-	showers::ShowerCreator shower_creator("./shower_file");
 	showers::Shower1D new_shower = shower_creator.create_shower(
 			pos,
 			2.e18,
-			1.,
-			1.,
+			0.75 * pi,
+			0.3 * pi,
 			1
 	);
 	std::vector<double> shower_x;
@@ -18,13 +21,17 @@ int main () {
 	std::vector<double> shower_z;
 	std::vector<double> shower_t;
 	std::vector<double> shower_ce;
+
 	new_shower.get_shower(
 		1.e-8,
+		&shower_t,
 		&shower_x,
 		&shower_y,
 		&shower_z,
-		&shower_t,
 		&shower_ce
 	);
-
+	TCanvas* c = new TCanvas("c", "Something", 0, 0, 800, 600);
+	auto graph1 = new TGraph(shower_t.size(), &shower_x[0], &shower_y[0]);
+	graph1 -> Draw();
+	c -> Print("plot_shower_profile.png");
 }
