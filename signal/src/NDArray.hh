fe048e6bf6c0fc03c6328e7ec09fcf7f57b57409
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2023-02-15 21:33:34 -0600

    make NDArray more dynamical

--- signal/src/NDArray.hh
+++ signal/src/NDArray.hh
@@ -1,76 +1,89 @@
 #ifndef __NDARRAY_HH
 #define __NDARRAY_HH
 
-#include <valarray>
-#include <functional>
+#include <vector>
+#include <array>
+#include <numeric>
+#include <algorithm>
 
-template <class T, std::size_t... Shape>
+template <class T, std::size_t dims>
 class NDArray {
 
 private:
-  using stride_t = std::valarray<std::size_t>;
-  using data_t = std::valarray<T>;
+
+  using shape_t = std::array<std::size_t, dims>;
+  using stride_t = std::array<std::size_t, dims + 1>;
+  using data_t = std::vector<T>;
 
 public:
-
-  NDArray(const T value) {
-    std::size_t seed = 1;
-    strides = {seed, seed *= Shape...};
-
-    data.resize(strides[strides.size() - 1], value);
+  
+  NDArray(const shape_t& shape, const T& value = T()) : m_shape(shape) {   
+    m_strides[0] = 1;
+    std::partial_sum(m_shape.begin(), m_shape.end(), m_strides.begin() + 1, std::multiplies<std::size_t>());
+    m_data.resize(m_strides.back(), value);
   }
 
-  NDArray(const NDArray<T, Shape...>& other) : strides(other.strides), data(other.data) {}
+  NDArray(const NDArray<T, dims>& other) : m_shape(other.m_shape), m_strides(other.m_strides), m_data(other.m_data) {}
 
   template <typename... Inds>
   T& operator()(Inds... inds) {
     std::size_t flat_ind = 0, dim = 0;
-    (..., (flat_ind += inds * strides[dim++]));
-
-    return data[flat_ind];
+    (..., (flat_ind += inds * m_strides[dim++]));
+    return m_data[flat_ind];
   }
 
-  friend NDArray<T, Shape...> operator+(const NDArray<T, Shape...>& lhs, const NDArray<T, Shape...>& rhs) {
-    return operator_binary(lhs, rhs, std::plus<data_t>());
+  friend NDArray<T, dims> operator+(const NDArray<T, dims>& lhs, const NDArray<T, dims>& rhs) {
+    return operator_binary(lhs, rhs, std::plus<T>());
   }
 
-  friend NDArray<T, Shape...> operator+(const NDArray<T, Shape...>& lhs, const T& rhs) {
-    return operator_binary(lhs, rhs, std::plus<data_t>());
+  friend NDArray<T, dims> operator+(const NDArray<T, dims>& lhs, const T& rhs) {
+    auto plus_rhs = [&](const T& el){return std::plus<T>()(el, rhs);};
+    return operator_unary(lhs, plus_rhs);
   }
 
-  friend NDArray<T, Shape...> operator-(const NDArray<T, Shape...>& lhs, const NDArray<T, Shape...>& rhs) {
-    return operator_binary(lhs, rhs, std::minus<data_t>());
+  friend NDArray<T, dims> operator-(const NDArray<T, dims>& lhs, const NDArray<T, dims>& rhs) {
+    return operator_binary(lhs, rhs, std::minus<T>());
   }
 
-  friend NDArray<T, Shape...> operator-(const NDArray<T, Shape...>& lhs, const T& rhs) {
-    return operator_binary(lhs, rhs, std::minus<data_t>());
+  friend NDArray<T, dims> operator-(const NDArray<T, dims>& lhs, const T& rhs) {
+    auto minus_rhs = [&](const T& el){return std::minus<T>()(el, rhs);};
+    return operator_unary(lhs, minus_rhs);
   }
 
-  friend NDArray<T, Shape...> operator*(const NDArray<T, Shape...>& lhs, const NDArray<T, Shape...>& rhs) {
-    return operator_binary(lhs, rhs, std::multiplies<data_t>());
+  friend NDArray<T, dims> operator*(const NDArray<T, dims>& lhs, const NDArray<T, dims>& rhs) {
+    return operator_binary(lhs, rhs, std::multiplies<T>());
   }
 
-  friend NDArray<T, Shape...> operator*(const NDArray<T, Shape...>& lhs, const T& rhs) {
-    return operator_binary(lhs, rhs, std::multiplies<data_t>());
+  friend NDArray<T, dims> operator*(const NDArray<T, dims>& lhs, const T& rhs) {
+    auto multiplies_rhs = [&](const T& el){return std::multiplies<T>()(el, rhs);};
+    return operator_unary(lhs, multiplies_rhs);
   }
 
-  friend NDArray<T, Shape...> operator/(const NDArray<T, Shape...>& lhs, const NDArray<T, Shape...>& rhs) {
-    return operator_binary(lhs, rhs, std::divides<data_t>());
+  friend NDArray<T, dims> operator/(const NDArray<T, dims>& lhs, const NDArray<T, dims>& rhs) {
+    return operator_binary(lhs, rhs, std::divides<T>());
   }
 
-  friend NDArray<T, Shape...> operator/(const NDArray<T, Shape...>& lhs, const T& rhs) {
-    return operator_binary(lhs, rhs, std::divides<data_t>());
+  friend NDArray<T, dims> operator/(const NDArray<T, dims>& lhs, const T& rhs) {
+    auto divides_rhs = [&](const T& el){return std::divides<T>()(el, rhs);};
+    return operator_unary(lhs, divides_rhs);
   }
 
 private:
 
-  stride_t strides;
-  data_t data;
+  shape_t m_shape = {};
+  stride_t m_strides = {};
+  data_t m_data = {};
 
-  friend NDArray<T, Shape...> operator_binary(const NDArray<T, Shape...>& lhs, const NDArray<T, Shape...>& rhs, 
-					      std::function<data_t(data_t, data_t)> op) {
-    NDArray<T, Shape...> result(lhs);
-    result.data = op(result.data, rhs.data);
+  friend NDArray<T, dims> operator_binary(const NDArray<T, dims>& lhs, const NDArray<T, dims>& rhs,
+					  std::function<T(const T&, const T&)> binary_op) {
+    NDArray<T, dims> result(lhs.m_shape);
+    std::transform(lhs.m_data.begin(), lhs.m_data.end(), rhs.m_data.begin(), result.m_data.begin(), binary_op);
+    return result;
+  }
+
+  friend NDArray<T, dims> operator_unary(const NDArray<T, dims>& arg, std::function<T(const T&)> unary_op) {
+    NDArray<T, dims> result(arg.m_shape);
+    std::transform(arg.m_data.begin(), arg.m_data.end(), result.m_data.begin(), unary_op);
     return result;
   }
 };
