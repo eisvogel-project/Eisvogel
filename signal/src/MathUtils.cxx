ec6666eb084238cba74c5d4f13580d6c1b5d1dc5
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2023-02-22 21:07:22 -0600

    prepare for better numerical stability

--- signal/src/MathUtils.cxx
+++ signal/src/MathUtils.cxx
@@ -6,7 +6,7 @@
 namespace MathUtils {
 
   // Algorithm from https://doi.org/10.2307/2346431
-  scalar_t incomplete_gamma(scalar_t a, scalar_t z, scalar_t rel_tol) {
+  scalar_t incomplete_gamma_series_expansion(scalar_t a, scalar_t z, scalar_t rel_tol) {
 
     scalar_t C = 1.0;
     scalar_t accum = C; 
@@ -20,5 +20,19 @@ namespace MathUtils {
     while(C / accum > rel_tol);
 
     return std::exp(std::lgamma(a)) * (1 - std::exp(a * std::log(z) - std::lgamma(a + 1) - z) * accum);
+  }
+
+  scalar_t incomplete_gamma_continued_fraction(scalar_t a, scalar_t z, scalar_t rel_tol) {
+    return 0.0;
+  }
+
+  scalar_t incomplete_gamma(scalar_t a, scalar_t z, scalar_t rel_tol) {    
+
+    if((z <= 1) || (z < a)) {
+      return incomplete_gamma_series_expansion(a, z, rel_tol);
+    }
+    else {
+      return incomplete_gamma_continued_fraction(a, z, rel_tol);
+    }    
   }
 }
--- signal/src/MathUtils.hh
+++ signal/src/MathUtils.hh
@@ -7,6 +7,9 @@ namespace MathUtils {
 
   scalar_t incomplete_gamma(scalar_t a, scalar_t z, scalar_t rel_tol = 1e-6);
 
+  scalar_t incomplete_gamma_series_expansion(scalar_t a, scalar_t z, scalar_t rel_tol = 1e-6);
+  scalar_t incomplete_gamma_continued_fraction(scalar_t a, scalar_t z, scalar_t rel_tol = 1e-6);
+
 };
 
 #endif
