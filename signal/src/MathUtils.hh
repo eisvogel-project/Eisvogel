20368fae36a6bf9971ed403b7b4eeb191a48d303
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2023-02-24 15:10:20 -0600

    use continued fraction representation for large argument values

--- signal/src/MathUtils.hh
+++ signal/src/MathUtils.hh
@@ -5,10 +5,10 @@
 
 namespace MathUtils {
 
-  scalar_t incomplete_gamma(scalar_t a, scalar_t z, scalar_t rel_tol = 1e-6);
+  scalar_t incomplete_gamma(scalar_t a, scalar_t z, scalar_t rel_tol = 1e-9);
 
-  scalar_t incomplete_gamma_series_expansion(scalar_t a, scalar_t z, scalar_t rel_tol = 1e-6);
-  scalar_t incomplete_gamma_continued_fraction(scalar_t a, scalar_t z, scalar_t rel_tol = 1e-6);
+  scalar_t incomplete_gamma_series_expansion(scalar_t a, scalar_t z, scalar_t rel_tol);
+  scalar_t incomplete_gamma_continued_fraction(scalar_t a, scalar_t z, scalar_t rel_tol);
 
 };
 
