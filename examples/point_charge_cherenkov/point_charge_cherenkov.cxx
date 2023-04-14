ac55a425064baacbfde5e345a2326f1c80a39854
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2023-04-14 16:39:48 -0500

    start dedicated folder with examples

--- examples/point_charge_cherenkov/point_charge_cherenkov.cxx
+++ examples/point_charge_cherenkov/point_charge_cherenkov.cxx
@@ -7,22 +7,47 @@
 #include "Kernels.hh"
 #include "SignalExport.hh"
 #include "Serialization.hh"
+#include "WeightingFieldUtils.hh"
+
+namespace WFU = WeightingFieldUtils;
+namespace CU = CoordUtils;
 
 int main(void) {
 
-  std::string path = "electric_dipole_wf.bin";
+  std::string wf_path = "electric_dipole_wf.bin";
+
   std::fstream ofs;
-  
-  ofs.open(path, std::ios::in | std::ios::binary);
-  stor::Serializer ser(ofs);
+  ofs.open(wf_path, std::ios::out | std::ios::binary);  
+  stor::Serializer oser(ofs);
+
+  // Domain of weighting field
+  CoordVector start_coords = CU::MakeCoordVectorTRZ(-20.0, -10.0, -40.0);
+  CoordVector end_coords = CU::MakeCoordVectorTRZ(320.0, 500.0, 40.0);
+
+  // Filter parameters
+  scalar_t tp = 5.0;
+  unsigned int N = 4;
+
+  // Sampling parameters
+  scalar_t os_factor = 7;
+  scalar_t r_min = 0.1;
+
+  std::cout << "Building weighting field ..." << std::endl;
+  WeightingField wf_out = WFU::CreateElectricDipoleWeightingField(start_coords, end_coords, tp, N, r_min, os_factor);
+
+  std::cout << "Saving weighting field ..." << std::endl;
+  oser.serialize(wf_out);
+  ofs.close();
+
+  std::fstream ifs; 
+  ifs.open(wf_path, std::ios::in | std::ios::binary);
+  stor::Serializer iser(ifs);
 
   std::cout << "Loading weighting field ..." << std::endl;
-  WeightingField wf = ser.deserialize<WeightingField>();
+  WeightingField wf_in = iser.deserialize<WeightingField>();
 
-  // SplineInterpolationKernelOrder1 interpolation_kernel;
-  // SplineInterpolationKernelOrder3 interpolation_kernel;
   KeysCubicInterpolationKernel interpolation_kernel;
-  Integrator integrator(wf, interpolation_kernel);
+  Integrator integrator(wf_in, interpolation_kernel);
 
   // test trajectory: a point charge moving parallel to the x-axis 
   // with a constant impact parameter of 'b' along the z-axis
