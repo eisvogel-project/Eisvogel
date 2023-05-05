8a4ea838eb23ab64c6daf24e0d97c4437d60ceda
Christoph <christophwelling@uchicago.edu>
2023-05-05 11:13:45 -0500

    Set up example for shower simulation

--- examples/shower_1D/shower_profile_1D.cpp
+++ examples/shower_1D/shower_profile_1D.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
 #include "Eisvogel/Common.hh"
 #include "Eisvogel/Integrator.hh"
@@ -10,6 +11,7 @@
 #include "Eisvogel/WeightingFieldUtils.hh"
 #include "shower_creator.h"
 #include "shower_1D.h"
+#include "constants.h"
 
 namespace WFU = WeightingFieldUtils;
 namespace CU = CoordUtils;
@@ -22,9 +24,44 @@ int main(void) {
   ofs.open(wf_path, std::ios::out | std::ios::binary);  
   stor::Serializer oser(ofs);
 
+  //std::array<float, 3> shower_vertex = {-76, .1, -63};
+  std::array<float, 3> shower_vertex = {-76, .1, -63};
+  std::cout << "Building Shower \n";
+
+  showers::ShowerCreator shower_creator("/home/welling/RadioNeutrino/scripts/Eisvogel/extern/shower_profile/shower_file");
+  showers::Shower1D shower = shower_creator.create_shower(
+          shower_vertex,
+          2.0e+19,
+          1.5708,
+          0,
+          1
+          );
+
+  std::vector<double> t;
+  std::vector<double> x;
+  std::vector<double> y;
+  std::vector<double> z;
+  std::vector<double> ce;
+  shower.get_shower(1., &t, &x, &y, &z, &ce);
+  double r_max = sqrt(x[0] * x[0] + y[0] * y[0] + z[0] * z[0]);
+  double r_min = sqrt(x[0] * x[0] + y[0] * y[0] + z[0] * z[0]);
+  double z_max = z[0];
+  double z_min = z[0];
+  double r;
+  for (int i=0; i<t.size(); i++) {
+      r = sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]);
+      r_min = std::min(r_min, r);
+      r_max = std::max(r_max, r);
+      z_min = std::min(z_min, z[i]);
+      z_max = std::max(z_max, z[i]);
+  }
+  r_max /= constants::c;
+  r_min /= constants::c;
+  z_max /= constants::c;
+  z_min /= constants::c;
   // Domain of weighting field
-  CoordVector start_coords = CU::MakeCoordVectorTRZ(-1000.0, -10, -50.0);
-  CoordVector end_coords = CU::MakeCoordVectorTRZ(1000.0, 1000.0, 50.0);
+  CoordVector start_coords = CU::MakeCoordVectorTRZ(-500, -100, z_min - 200);
+  CoordVector end_coords = CU::MakeCoordVectorTRZ(1000.0, r_max + 800, z_max + 200);
 
   // Filter parameters
   scalar_t tp = 5.0;
@@ -32,10 +69,19 @@ int main(void) {
 
   // Sampling parameters
   scalar_t os_factor = 7;
-  scalar_t r_min = 0.1;
+  scalar_t R_min = 0.1;
+  scalar_t refractive_index = 1.3;
 
   std::cout << "Building weighting field ..." << std::endl;
-  WeightingField wf_out = WFU::CreateElectricDipoleWeightingField(start_coords, end_coords, tp, N, r_min, os_factor);
+  WeightingField wf_out = WFU::CreateElectricDipoleWeightingField(
+          start_coords,
+          end_coords,
+          tp,
+          N,
+          R_min,
+          os_factor,
+          refractive_index
+          );
 
   std::cout << "Saving weighting field ..." << std::endl;
   oser.serialize(wf_out);
@@ -58,26 +104,14 @@ int main(void) {
   scalar_t charge = 1;
   scalar_t beta = 0.9;
   
-  std::array<float, 3> shower_vertex = {0.1, 0.1, -10};
-  std::cout << "Building Shower \n";
-
-  showers::ShowerCreator shower_creator("/home/windischhofer/Eisvogel/Eisvogel/extern/shower_profile/shower_file");
-  showers::Shower1D shower = shower_creator.create_shower(
-          shower_vertex,
-          5.0e+17,
-          1.5708,
-          0,
-          true
-          );
-
+  
   std::vector<scalar_t> signal_times, signal_values;
-  std::cout << "Interating \n";
-  for(scalar_t cur_t = 30; cur_t < 1000; cur_t += 10) {
-      std::cout << "Calculating t=" << cur_t << "\n";
+  std::cout << "Integrating \n";
+  for(scalar_t cur_t = 0; cur_t < 800; cur_t += 1) {
     scalar_t cur_signal = integrator.integrate(cur_t, shower, 1);
     signal_times.push_back(cur_t);
     signal_values.push_back(cur_signal);
   }
-  ExportSignal(signal_times, signal_values, "./test_signal.csv");
+  ExportSignal(signal_times, signal_values, "./2e19HAD.csv");
   return 0;
 }
