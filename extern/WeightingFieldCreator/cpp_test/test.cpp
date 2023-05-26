569b93472859f04a6b821a31badc22330e8f7718
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2023-05-26 14:22:57 -0500

    move to cylindrical coordinates

--- extern/WeightingFieldCreator/cpp_test/test.cpp
+++ extern/WeightingFieldCreator/cpp_test/test.cpp
@@ -3,7 +3,7 @@
 using namespace meep;
 
 double eps(const vec &p) {
-  if (p.x() < 2 && p.y() < 3)
+  if (p.z() < 5)
     return 12.0;
   return 1.0;
 }
@@ -13,17 +13,19 @@ std::complex<double> srcfunc(double t, void*) {
 }
 
 int main(int argc, char **argv) {
-  initialize mpi(argc, argv); // do this even for non-MPI Meep
-  double resolution = 20;     // pixels per distance
-  grid_volume v = vol2d(5, 10, resolution); // 5x10 2d cell
+  initialize mpi(argc, argv);
+  double resolution = 20;
+  grid_volume v = volcyl(5, 10, resolution); // (r, z, resolution)
+  
   structure s(v, eps, pml(1.0));
   fields f(&s);
 
   f.output_hdf5(Dielectric, v.surroundings());
 
   custom_src_time src(srcfunc, NULL);
-  f.add_point_source(Ez, src, vec(1.1, 2.3));
-  while (f.time() < 20) {
+  f.add_point_source(Ez, src, veccyl(0.0, 4.0));
+  
+  while (f.time() < 13) {
      f.step();
   }
 
