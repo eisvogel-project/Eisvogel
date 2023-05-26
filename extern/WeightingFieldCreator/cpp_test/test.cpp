0bebe92b43a08b247fff6f9bd0f99cf55be13164
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2023-05-26 15:06:11 -0500

    meep settings for nice plots

--- extern/WeightingFieldCreator/cpp_test/test.cpp
+++ extern/WeightingFieldCreator/cpp_test/test.cpp
@@ -3,29 +3,35 @@
 using namespace meep;
 
 double eps(const vec &p) {
-  if (p.z() < 5)
-    return 12.0;
+  if (p.z() < 10)
+    return 2.0;
   return 1.0;
 }
 
 std::complex<double> srcfunc(double t, void*) {
-  return std::exp(-50 * std::pow(t - 10, 2));
+  if(t > 20) {
+    return 0.0;
+  }
+  else {
+    return std::exp(-50 * std::pow(t - 10, 2));
+  }
+  return 0.0;
 }
 
 int main(int argc, char **argv) {
   initialize mpi(argc, argv);
   double resolution = 20;
-  grid_volume v = volcyl(5, 10, resolution); // (r, z, resolution)
+  grid_volume v = volcyl(20, 20, resolution); // (r, z, resolution)
   
-  structure s(v, eps, pml(1.0));
+  structure s(v, eps, pml(1.0), identity(), 0, 0.5);
   fields f(&s);
 
   f.output_hdf5(Dielectric, v.surroundings());
 
   custom_src_time src(srcfunc, NULL);
-  f.add_point_source(Ez, src, veccyl(0.0, 4.0));
+  f.add_point_source(Ez, src, veccyl(0.0, 8.0));
   
-  while (f.time() < 13) {
+  while (f.time() < 20) {
      f.step();
   }
 
