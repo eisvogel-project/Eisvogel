b29dc9d2b59e080a440e13173995a01e6562a25b
Philipp Windischhofer <philipp.windischhofer@gmail.com>
2023-06-06 11:19:54 -0500

    Revert "1d shower emission"

--- extern/shower_profile/environment/ice_profile.cpp
+++ extern/shower_profile/environment/ice_profile.cpp
@@ -1,8 +1,7 @@
 #include "ice_profile.h"
-#include "units.h"
 
 double environment::IceProfile::get_density(float x, float y, float z) {
-	return 917 * units::kg / units::cubic_meter;
+	return 917.0;
 }
 double environment::IceProfile::get_index_of_refraction(float x, float y, float z) {
 	return 1.77;
