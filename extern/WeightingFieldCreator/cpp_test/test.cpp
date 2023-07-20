5d6bd5eafee635f7eea6f18b0a9907d50fee4705
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2023-07-20 09:35:19 -0500

    record latest setup

--- extern/WeightingFieldCreator/cpp_test/test.cpp
+++ extern/WeightingFieldCreator/cpp_test/test.cpp
@@ -3,12 +3,14 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <filesystem>
+#include <utility>
 #include "H5Cpp.h"
 using namespace meep;
 
 double eps(const vec &p) {
-  if (p.z() < 5)
-    return 4.0;
+  if (p.z() < 20)
+    return 1.78;
   return 1.0;
 }
 
@@ -24,8 +26,8 @@ unsigned int fact(unsigned arg) {
 
 std::complex<double> srcfunc(double t, void*) {
 
-  unsigned int order = 4;
-  double tp = 2.0;
+  unsigned int order = 6;
+  double tp = 1.0;
   
   if(t > 0) {
     double retval = 1.0 / (tp * fact(order - 1)) * std::pow(t * (double)order / tp, (double)order) * std::exp(-t * (double)order / tp);
@@ -40,15 +42,23 @@ void my_chunkloop(fields_chunk *fc, int ichunk, component cgrid, ivec is, ivec i
 		  const symmetry &S, int sn, void *chunkloop_data)
 {
 
+  // make sure to give enough flexibility to store different fields / field combinations
+  
   std::cout << "creating file" << std::endl;
 
-  H5::H5File file("output_chunk_" + std::to_string(ichunk) + ".h5", H5F_ACC_TRUNC);
-
-  std::ofstream asciifile;
-  asciifile.open("output_chunk_" + std::to_string(ichunk) + ".txt");
+  std::string out_dir((char*)chunkloop_data);
   
+  std::cout << "out_dir = " << out_dir << std::endl;
+  
+  H5::H5File file(out_dir + "/output_chunk_" + std::to_string(ichunk) + ".h5", H5F_ACC_TRUNC);
+
+  // index vectors for start and end of chunk
   ivec isS = S.transform(is, sn) + shift;
   ivec ieS = S.transform(ie, sn) + shift;
+
+  // need to:
+  // * determine actual dimensionality of field (2? 3? --> in meep-core everything seems to be always 3d)
+  // * fill stride and dims
   
   size_t bufsz = 1;
   size_t dims[2];
@@ -107,9 +117,9 @@ void my_chunkloop(fields_chunk *fc, int ichunk, component cgrid, ivec is, ivec i
     ivec ichild = S.transform(iparent, sn) + shift;
     vec rchild = S.transform(rparent, sn) + rshift;
 
-    std::cout << "chunk_number = " << ichunk << std::endl;
-    std::cout << "r_ind = " << ichild.r() << " z_ind = " << ichild.z() << std::endl;
-    std::cout << "r = " << rchild.r() << " z = " << rchild.z() << std::endl;
+    // std::cout << "chunk_number = " << ichunk << std::endl;
+    // std::cout << "r_ind = " << ichild.r() << " z_ind = " << ichild.z() << std::endl;
+    // std::cout << "r = " << rchild.r() << " z = " << rchild.z() << std::endl;
     
     // fetch field components at child point
     data.update_values(idx);
@@ -117,34 +127,30 @@ void my_chunkloop(fields_chunk *fc, int ichunk, component cgrid, ivec is, ivec i
     std::complex<double> Ey_val = data.values[1];
     std::complex<double> Ez_val = data.values[2];
 
-    std::cout << "loop_i1 = " << loop_i1 << std::endl;
-    std::cout << "loop_i2 = " << loop_i2 << std::endl;
-    std::cout << "loop_i3 = " << loop_i3 << std::endl;
+    // std::cout << "loop_i1 = " << loop_i1 << std::endl;
+    // std::cout << "loop_i2 = " << loop_i2 << std::endl;
+    // std::cout << "loop_i3 = " << loop_i3 << std::endl;
     
     ptrdiff_t idx2 = loop_i1 * stride[0] + loop_i2 * stride[2] + loop_i3 * stride[1];
-    std::cout << "buff_idx (bufsz) = " << idx2 << " (" << bufsz << ")" << std::endl;
-    std::cout << Ez_val.real() << std::endl;
+    // std::cout << "buff_idx (bufsz) = " << idx2 << " (" << bufsz << ")" << std::endl;
+    // std::cout << Ez_val.real() << std::endl;
     buff[idx2] = Ez_val.real();
 
-    std::cout << "- - -" << std::endl;
-
-    asciifile << Ez_val.real() << " " << std::endl;
+    // std::cout << "- - -" << std::endl;
   }
 
   std::cout << "writing chunk ...";
 
   dataset.write(buff, H5::PredType::NATIVE_DOUBLE);
   metadata.write(metabuff, H5::PredType::NATIVE_INT);
-  file.close();
-  
-  asciifile.close();
+  file.close(); 
 }
 
 int main(int argc, char **argv) {
   
   initialize mpi(argc, argv);
-  double resolution = 2;
-  grid_volume gv = volcyl(10, 10, resolution); // (r, z, resolution)
+  double resolution = 20;
+  grid_volume gv = volcyl(40, 40, resolution); // (r, z, resolution)
   
   structure s(gv, eps, pml(1.0), identity(), 0, 0.5);
   fields f(&s);
@@ -152,18 +158,21 @@ int main(int argc, char **argv) {
   f.output_hdf5(Dielectric, gv.surroundings());
 
   custom_src_time src(srcfunc, NULL);
-  f.add_point_source(Ez, src, veccyl(0.0, 3.0));
+  f.add_point_source(Ez, src, veccyl(0.0, 15.0));
+  //f.add_point_source(Ez, src, veccyl(0.0, 20.0));
 
-  //for(double cur_t = 2.0; cur_t <= 40; cur_t += 2) {
-  double cur_t = 10;
-  while (f.time() < cur_t) {
-    f.step();
-  }
-    
-  f.loop_in_chunks(my_chunkloop, NULL, f.total_volume());
+  for(double cur_t = 2.0; cur_t <= 40; cur_t += 2) {
+    //double cur_t = 10;
+    while (f.time() < cur_t) {
+      f.step();
+    }
+
+    std::string out_dir = "output/t_" + std::to_string(cur_t);
+    std::filesystem::create_directories(out_dir);
+    f.loop_in_chunks(my_chunkloop, (void*)out_dir.c_str(), f.total_volume());
   
-  f.output_hdf5(Ez, gv.surroundings());
-    //}
+    f.output_hdf5(Ez, gv.surroundings());
+  }
   
   return 0;
 }
