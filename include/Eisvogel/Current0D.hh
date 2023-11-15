b61cddd8e5667a68266ea12b962ba8018bb0a56b
Philipp Windischhofer <windischhofer@uchicago.edu>
2023-11-15 17:06:03 -0600

    drop changes in empty lines

--- include/Eisvogel/Current0D.hh
+++ include/Eisvogel/Current0D.hh
@@ -9,14 +9,13 @@ class Current0D {
 public:
 
   Current0D() : m_points({}), m_charges({}) { };
-  
   Current0D(std::vector<CoordVector>&& points, std::vector<scalar_t>&& charges) : m_points(points), 
 										  m_charges(charges) { 
     if(m_points.size() != m_charges.size() + 1) {
       throw;
     }
   };
-  
+
   std::size_t number_segments() const {return m_charges.size();}
 
   const scalar_t& GetCharge(std::size_t ind) const {return m_charges[ind];}
