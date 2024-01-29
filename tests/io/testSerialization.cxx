7f57e8b675e106d7003bf29dccd66606383236ea
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-01-29 17:43:28 -0600

    keep track of timing

--- tests/io/testSerialization.cxx
+++ tests/io/testSerialization.cxx
@@ -3,14 +3,24 @@
 
 #include <fstream>
 #include <iostream>
+#include <chrono>
+#include <random>
 
-int main(int argc, char* argv[]) {
+int test_serialization_vector(std::string ser_path, std::size_t length) {
 
   using vector_t = std::vector<scalar_t>;
-  
-  vector_t vec = {-1, -2, 3, 4};
 
-  std::string ser_path = "ser_test.bin";
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(1.0, 2.0);
+  
+  // Generate random vector
+  vector_t vec;
+  for(std::size_t ind = 0; ind < length; ind++) {
+    vec.push_back(dis(gen));
+  }
+
+  std::chrono::high_resolution_clock::time_point t_start = std::chrono::high_resolution_clock::now();
   
   std::fstream ofs;
   ofs.open(ser_path, std::ios::out | std::ios::binary);  
@@ -20,12 +30,26 @@ int main(int argc, char* argv[]) {
 
   std::fstream ifs;
   ifs.open(ser_path, std::ios::in | std::ios::binary);
-  stor::Serializer iser(ifs);  
-  
+  stor::Serializer iser(ifs);    
   vector_t res = iser.deserialize<vector_t>();
-  ifs.close();
+  ifs.close();  
 
-  for(auto cur: res) {
-    std::cout << cur << std::endl;
+  std::chrono::high_resolution_clock::time_point t_end = std::chrono::high_resolution_clock::now();
+
+  std::chrono::duration<double> time_span = duration_cast<std::chrono::duration<double>>(t_end - t_start);
+  std::cout << "It took me " << time_span.count() << " seconds." << std::endl;
+  
+  for(std::size_t ind = 0; ind < length; ind++) {
+    if(vec[ind] != res[ind]) {
+      throw std::runtime_error("Error: mistake");
+    }
   }
+
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+       
+  std::string ser_path = "ser_test.bin";
+  test_serialization_vector(ser_path, 100000);
 }
