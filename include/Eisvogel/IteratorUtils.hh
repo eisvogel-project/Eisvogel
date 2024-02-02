2f529e158a274b7b440188dea6c05c3c0dbf26e4
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-02-02 08:37:58 -0600

    remove code duplication

--- include/Eisvogel/IteratorUtils.hh
+++ include/Eisvogel/IteratorUtils.hh
@@ -3,18 +3,18 @@
 
 #include "NDArray.hh"
 
-class IndexCounter {
+template <typename VecT> class VectorCounter {
 
 private:
 
-  IndexVector m_start, m_end, m_cur;
+  VecT m_start, m_end, m_cur;
   std::size_t number_dims;
 
 public:
 
-  IndexCounter(const IndexVector& start, const IndexVector& end) : m_start(start), m_end(end), m_cur(start), 
-								   number_dims(start.size()) { }
-  IndexCounter& operator++() {
+  VectorCounter(const VecT& start, const VecT& end) : m_start(start), m_end(end), m_cur(start), 
+						     number_dims(start.size()) { }
+  VectorCounter& operator++() {
     for(size_t i = 0; i < number_dims; i++) {
       if(++m_cur(i) == m_end(i)) {
 	if(i < number_dims - 1) {
@@ -32,51 +32,16 @@ public:
     return m_cur(number_dims - 1) < m_end(number_dims - 1);
   }
 
-  IndexVector::type& operator()(size_t ind) {return m_cur(ind);}
+  VecT::type& operator()(size_t ind) {return m_cur(ind);}
 
   auto begin() {return m_cur.begin();}
   auto end() {return m_cur.end();}
 
-  inline IndexVector& index() {return m_cur;}
+  inline VecT& index() {return m_cur;}
 };
 
-
-class GridCounter {
-
-private:
-
-  GridVector m_start, m_end, m_cur;
-  std::size_t number_dims;
-
-public:
-
-  GridCounter(const GridVector& start, const GridVector& end) : m_start(start), m_end(end), m_cur(start), 
-								   number_dims(start.size()) { }
-  GridCounter& operator++() {
-    for(size_t i = 0; i < number_dims; i++) {
-      if(++m_cur(i) == m_end(i)) {
-	if(i < number_dims - 1) {
-	  m_cur(i) = m_start(i);
-	}
-      }
-      else
-	break;
-    }
-
-    return *this;
-  }
-
-  bool running() {
-    return m_cur(number_dims - 1) < m_end(number_dims - 1);
-  }
-
-  GridVector::type& operator()(size_t ind) {return m_cur(ind);}
-
-  auto begin() {return m_cur.begin();}
-  auto end() {return m_cur.end();}
-
-  inline GridVector& index() {return m_cur;}
-};
+using IndexCounter = VectorCounter<IndexVector>;
+using GridCounter = VectorCounter<GridVector>;
 
 // some additional utility functions
 bool isInIndexRange(const IndexVector& inds, const IndexVector& start_inds, const IndexVector& stop_inds);
