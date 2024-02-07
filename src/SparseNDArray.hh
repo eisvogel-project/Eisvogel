923e4d5413deedacef58c2da0ac7329b134c6bed
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-02-07 11:39:54 -0600

    first and rough implementation of SparseNDArray

--- src/SparseNDArray.hh
+++ src/SparseNDArray.hh
@@ -1,15 +1,102 @@
 #ifndef __SPARSE_NDARRAY_HH
 #define __SPARSE_NDARRAY_HH
 
+#include <array>
+#include <map>
 #include "NDArray.hh"
+#include "DenseNDArray.hh"
+#include "Eisvogel/IteratorUtils.hh"
 
 template <class T, std::size_t dims>
 class SparseNDArray : public NDArray<T, dims> {
 
+private:
+  friend struct stor::Traits<SparseNDArray<T, dims>>;
+  
 public:
   using shape_t = typename NDArray<T, dims>::shape_t;
+  using index_t = std::array<std::size_t, dims>;
+  using data_t = std::map<index_t, T>;
+
+  SparseNDArray(const shape_t& shape, const T& default_value) : NDArray<T, dims>(shape), m_default_value(default_value) { }
+
+  template<typename KeeperT>
+  static SparseNDArray<T, dims> FromDense(const DenseNDArray<T, dims>& dense_arr, KeeperT to_keep, const T& default_value) {
+
+    SparseNDArray<T, dims> retval(dense_arr.shape(), default_value);
+
+    IndexVector start_inds(dims, 0);
+    IndexVector end_inds = dense_arr.shape();
+    for(IndexCounter cnt(start_inds, end_inds); cnt.running(); ++cnt) {
+
+      IndexVector cur_ind = cnt.index();
+      T cur_val = dense_arr(cur_ind);
+
+      if(to_keep(cur_val)) {
+	retval.set(cur_ind, cur_val);
+      }
+    }
+    
+    return retval;    
+  }
+
+  T& operator()(IndexVector& inds) {
+
+    // TODO: this will be changed when the fixed-dim vectors are available
+    // Can then simply access the internal data of the fixed-dim vector class instead
+    // of having to build a new index all the time like this
+    index_t key;
+    for(std::size_t dim = 0; dim < dims; dim++) {
+      key[dim] = inds(dim);
+    }
+    
+    auto it = m_data.find(key);
+    if(it == m_data.end()) {
+        return m_default_value;
+    }
+    return it -> second;
+  }
+
+private:
+
+  void set(IndexVector& inds, T value) {
+    
+    index_t key;
+    for(std::size_t dim = 0; dim < dims; dim++) {
+      key[dim] = inds(dim);
+    }
+    
+    m_data[key] = value;
+  }
   
+  T m_default_value;
+  data_t m_data = {};  
 };
+
+namespace stor {
+
+  template<typename T, std::size_t dims>
+  struct Traits<SparseNDArray<T, dims>> {
+    using type = SparseNDArray<T, dims>;
+    using shape_t = typename type::shape_t;
+    using data_t = typename type::data_t;
+
+    static void serialize(std::iostream& stream, const type& val) {
+      Traits<T>::serialize(stream, val.m_default_value);
+      Traits<shape_t>::serialize(stream, val.m_shape);
+      Traits<data_t>::serialize(stream, val.m_data);
+    }
+
+    static type deserialize(std::iostream& stream) {
+      T default_value = Traits<T>::deserialize(stream);
+      shape_t shape = Traits<shape_t>::deserialize(stream);
+      data_t data = Traits<data_t>::deserialize(stream);
+      SparseNDArray<T, dims> retval(shape, default_value);
+      retval.m_data = data;
+      return retval;
+    }
+  };
+}
 
 // Some type shortcuts
 template <class T>
--- tests/internals/testSparseNDArray.cxx
+++ tests/internals/testSparseNDArray.cxx
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include "Serialization.hh"
+#include "DenseNDArray.hh"
 #include "SparseNDArray.hh"
 
 int main(int argc, char* argv[]) {
 
+  DenseNDArray<float, 2> darr({3, 3}, 0.0);
+
+  darr(0, 0) = 1.0;
+  darr(1, 0) = 0.4;
+  darr(1, 1) = 1.4;
+  
+  auto to_keep = [](float value) -> bool {
+    if(value > 1e-6) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+  
+  SparseNDArray<float, 2> sparr = SparseNDArray<float, 2>::FromDense(darr, to_keep, 0.0);
+
+  std::string ser_path = "sparse_ser_test.bin";
+  
+  std::fstream ofs;
+  ofs.open(ser_path, std::ios::out | std::ios::binary);  
+  stor::DefaultSerializer oser;
+  oser.serialize(ofs, sparr);
+  ofs.close();
+
+  std::fstream ifs;
+  ifs.open(ser_path, std::ios::in | std::ios::binary);
+  stor::DefaultSerializer iser; 
+  SparseNDArray<float, 2> sparr_res = iser.deserialize<SparseNDArray<float, 2>>(ifs);
+  ifs.close();
+  
+  IndexVector acc_ind1 = {1,1};
+  std::cout << sparr_res(acc_ind1) << std::endl;
   
   std::cout << "done" << std::endl;
 }
