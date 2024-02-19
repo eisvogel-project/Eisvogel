8a14cac4022316aac5f7a9d17b47116514b1f0da
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-02-19 15:27:52 -0600

    cherenkov example for particle moving along z-axis

--- examples/point_charge_cherenkov/point_charge_cherenkov.cxx
+++ examples/point_charge_cherenkov/point_charge_cherenkov.cxx
@@ -15,25 +15,25 @@ int main(int argc, char* argv[]) {
   std::string wf_path = argv[1];
   SignalCalculator calc(wf_path);
 
-  // test trajectory: a point charge moving parallel to the x-axis 
-  // with a constant impact parameter of 'b' along the z-axis
-  scalar_t b = 20;
-  scalar_t tstart = -250, tend = 250;
+  // test trajectory: a point charge moving parallel to the z-axis 
+  // with a constant impact parameter of 'b' along the x-axis
+  scalar_t b = 50;
+  scalar_t tstart = -100, tend = 100;
   scalar_t charge = 1;
   scalar_t beta = 1.2;
 
   std::cout << "Building trajectory ..." << std::endl;
   Current0D track({
-      CoordUtils::MakeCoordVectorTXYZ(tstart, beta * tstart, 0, b),
-  	CoordUtils::MakeCoordVectorTXYZ(tend, beta * tend, 0, b)
-  	},
+      CoordUtils::MakeCoordVectorTXYZ(tstart, b, 0, beta * tstart),
+      CoordUtils::MakeCoordVectorTXYZ(tend, b, 0, beta * tend)
+    },
     {charge}
     );
 
   std::cout << "Computing signal ..." << std::endl;
 
   std::vector<scalar_t> signal_times, signal_values;
-  for(scalar_t cur_t = 0; cur_t < 40; cur_t += 1) {
+  for(scalar_t cur_t = 10; cur_t < 45; cur_t += 0.1) {
     scalar_t cur_signal = calc.ComputeSignal(track, cur_t);
     signal_times.push_back(cur_t);
     signal_values.push_back(cur_signal);
