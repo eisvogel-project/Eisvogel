09c18504977ec0a26f7808a8b12d319a57c1da24
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-03-13 08:43:09 -0500

    deep dipole batch running

--- examples/dipole_ice/run_dipole_ice.sh
+++ examples/dipole_ice/run_dipole_ice.sh
@@ -5,4 +5,4 @@ source ${EISVOGELDIR}/setup_mdwy.sh
 
 cd ${EISVOGELDIR}/build/
 source ./setup.sh
-mpirun --mca orte_base_help_aggregate 0 -np 64 examples/dipole_ice/dipole_ice /home/windischhofer/data/windischhofer/eisvogel/wf_dipole_ice_meep_exp_ratio_20_300
+mpirun --mca orte_base_help_aggregate 0 -np 64 examples/dipole_ice/dipole_ice /home/windischhofer/data/windischhofer/eisvogel/wf_dipole_ice_deep_meep_exp_ratio_20_300
