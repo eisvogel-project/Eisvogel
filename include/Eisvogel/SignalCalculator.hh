1da89160340cd7730d7f173113241c1f9292ec3d
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-03-14 12:38:17 -0500

    some interface documentation

--- include/Eisvogel/SignalCalculator.hh
+++ include/Eisvogel/SignalCalculator.hh
@@ -14,6 +14,16 @@ public:
   SignalCalculator(const std::filesystem::path& geometry_path);
   scalar_t ComputeSignal(const Current0D& track, scalar_t t_sig);
   scalar_t ComputeSignal(const SparseCurrentDensity3D& current_distribution, scalar_t t_sig);
+
+   /**
+     * Calculate the contribution to the antenna signal arising from a particle track.
+     *
+     * @param track    The particle track.
+     * @param ts       The time values at which the signal should be calculated
+     * @param signal   Antenna signal buffer in which the contribution is to be accumulated.
+     *                 Must exist and have the same length as ts.
+     */  
+  void AccumulateSignal(const Current0D& track, std::vector<scalar_t>& ts, std::vector<scalar_t>& signal);
   
 private:
   std::filesystem::path m_geometry_path;
