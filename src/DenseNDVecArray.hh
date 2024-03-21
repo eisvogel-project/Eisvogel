f11aecda2fcc21b8213cc153b97a0f4beab088c7
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-03-21 18:16:03 -0500

    view access to vec array

--- src/DenseNDVecArray.hh
+++ src/DenseNDVecArray.hh
@@ -40,28 +40,60 @@ private:
   // constructor used by deserializer
   DenseNDVecArray(const shape_t& shape, const stride_t& strides, const std::size_t offset, data_t&& data) :
     m_shape(shape), m_strides(strides), m_offset(offset), m_data(std::make_shared<data_t>(data)) { }
+
+  DenseNDVecArray(const shape_t& shape, const stride_t& strides, const std::size_t offset, std::shared_ptr<data_t> data) :
+    m_shape(shape), m_strides(strides), m_offset(offset), m_data(data) { }
   
 public:
   
   DenseNDVecArray(const shape_t& shape, const T& value) : m_offset(0), m_shape(shape) {
-    m_strides[0] = 1;
-    std::partial_sum(shape.begin(), shape.end(), m_strides.begin() + 1, std::multiplies<std::size_t>());
-    m_strides *= vec_dims;
-
-    // reverse strides so that slowest-changing index is the last one
-    std::reverse(std::execution::unseq, m_strides.begin(), m_strides.end());
+    m_strides = ComputeStrides(shape);
     
     // reserve the required memory
     m_data = std::make_shared<data_t>(GetVolume(), value);
-  } 
+  }   
   
+  // Single-element access
   view_t operator[](const ind_t& ind) {
     std::size_t flat_ind = std::inner_product(ind.cbegin(), ind.cend(), m_strides.begin() + 1, m_offset);
     return view_t(m_data -> begin() + flat_ind, vec_dims);
   }
+
+  view_t operator[](const ind_t& ind) requires(dims == 2) {
+    std::size_t flat_ind = m_offset + ind[0] * m_strides[1] + ind[1] * m_strides[2];
+    return view_t(m_data -> begin() + flat_ind, vec_dims);
+  }
+  
+  view_t operator[](const ind_t& ind) requires(dims == 3) {
+    std::size_t flat_ind = m_offset + ind[0] * m_strides[1] + ind[1] * m_strides[2] + ind[2] * m_strides[3];
+    return view_t(m_data -> begin() + flat_ind, vec_dims);
+  }
+
+  // Array view access
+  DenseNDVecArray<T, dims, vec_dims> View(const ind_t& start_ind, const ind_t& end_ind) {
+    shape_t view_shape = end_ind - start_ind;
+    stride_t view_strides = ComputeStrides(view_shape);        
+    std::size_t view_offset = std::inner_product(start_ind.cbegin(), start_ind.cend(), m_strides.begin() + 1, m_offset);
+
+    return DenseNDVecArray<T, dims, vec_dims>(view_shape, view_strides, view_offset, m_data);
+  }
   
   const std::size_t GetVolume() const {return m_strides[0];} 
-    
+
+private:
+
+  static stride_t ComputeStrides(const shape_t& shape) {
+    stride_t strides;
+    strides[0] = 1;
+    std::partial_sum(shape.begin(), shape.end(), strides.begin() + 1, std::multiplies<std::size_t>());
+    strides *= vec_dims;
+
+    // reverse strides so that slowest-changing index is the last one
+    std::reverse(std::execution::unseq, strides.begin(), strides.end());
+  
+    return strides;
+  }
+  
 private:
   
   std::shared_ptr<data_t> m_data;
--- tests/internals/testVector.cxx
+++ tests/internals/testVector.cxx
@@ -13,46 +13,53 @@
 
 int main(int argc, char* argv[]) {  
 
-  std::shared_ptr<std::vector<float>> vec = std::make_shared<std::vector<float>>(10);
-
-  std::span<float, 3> view(vec -> begin() + 2, 3);
-  view[0] = 100;
-  view[1] = 101;
-  view[2] = 102;
-    
-  Vector<float, 3> vec1(view);
-
-  std::cout << "vec1 = " << std::endl;
-  std::cout << vec1[0] << std::endl;
-  std::cout << vec1[1] << std::endl;
-  std::cout << vec1[2] << std::endl;
-
-  DenseNDVecArray<float, 2, 3> arr1({2u, 2u}, 0.0f);
-  Vector<std::size_t, 2> ind{1u, 1u};  
-  arr1[ind] = vec1;
-
-  std::filesystem::path testpath = "./testVector.bin";   
-  std::fstream ofs;
-  ofs.open(testpath, std::ios::out | std::ios::binary);  
-  stor::DefaultSerializer oser;
-  oser.serialize(ofs, arr1);
-  ofs.close();
-
-  std::fstream ifs;
-  ifs.open(testpath, std::ios::in | std::ios::binary);
-  stor::DefaultSerializer iser;
-  DenseNDVecArray<float, 2, 3> arr1_read = iser.deserialize<DenseNDVecArray<float, 2, 3>>(ifs);
+  Vector<float, 2> vec1{1.f, 2.f};
   
-  auto value = arr1_read[ind];
-  std::cout << "retrieved value =" << std::endl;
-  for(auto cur: value) {
+  // 3-dim array of 2-dim (field) vectors
+  DenseNDVecArray<float, 3, 2> arr1({400u, 400u, 400u}, 0.0f);
+  
+  Vector<std::size_t, 2> ind{1u, 1u};
+  arr1[{350u, 350u, 1u}] = vec1;
+
+  std::cout << "empty value =" << std::endl;
+  for(auto cur: arr1[{0u, 0u, 0u}]) {
+    std::cout << cur << std::endl;
+  }  
+
+  std::cout << "filled value =" << std::endl;
+  for(auto cur: arr1[{350u, 350u, 1u}]) {
     std::cout << cur << std::endl;
   }
 
-  std::cout << "empty value =" << std::endl;
-  for(auto cur: arr1_read[{0u, 0u}]) {
+  DenseNDVecArray<float, 3, 2> arr_view = arr1.View({350u, 350u, 1u}, {351u, 351u, 10u});
+
+  std::cout << "empty value in view =" << std::endl;
+  for(auto cur: arr_view[{0u, 0u, 3u}]) {
     std::cout << cur << std::endl;
-  }  
+  }
+  
+  std::cout << "filled value in view =" << std::endl;
+  for(auto cur: arr_view[{0u, 0u, 0u}]) {
+    std::cout << cur << std::endl;
+  }
+  
+  // std::filesystem::path testpath = "./testVector.bin";   
+  // std::fstream ofs;
+  // ofs.open(testpath, std::ios::out | std::ios::binary);  
+  // stor::DefaultSerializer oser;
+  // oser.serialize(ofs, arr1);
+  // ofs.close();
+
+  // std::fstream ifs;
+  // ifs.open(testpath, std::ios::in | std::ios::binary);
+  // stor::DefaultSerializer iser;
+  // DenseNDVecArray<float, 2, 3> arr1_read = iser.deserialize<DenseNDVecArray<float, 2, 3>>(ifs);
+  
+  // auto value = arr1_read[ind];
+  // std::cout << "retrieved value =" << std::endl;
+  // for(auto cur: value) {
+  //   std::cout << cur << std::endl;
+  // }
   
   std::cout << "done" << std::endl;
 }
