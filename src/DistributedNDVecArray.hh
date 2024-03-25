3edc6d66b2841f489fcf32379fa8069da3cce47a
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-03-24 21:23:23 -0500

    prepare next steps

--- src/DistributedNDVecArray.hh
+++ src/DistributedNDVecArray.hh
@@ -1,2 +1,30 @@
 #pragma once
 
+#include <filesystem>
+#include <fstream>
+
+#include "DenseNDVecArrayStreamer.hh"
+
+class ChunkIndex {
+
+public:
+
+  
+  
+};
+
+template <std::size_t dims, std::size_t vec_dims>
+class DistributedNDVecArray {
+
+public:
+
+  DistributedNDVecArray(std::filesystem::path dirpath, std::size_t cache_size);
+
+private:
+
+  const std::filesystem::path m_dirpath;
+  const std::size_t m_cache_size;
+  
+};
+
+#include "DistributedNDVecArray.hxx"
