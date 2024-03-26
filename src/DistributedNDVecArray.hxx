ee285fe5a5331a2be1b3590a90f726960d85ae92
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-03-26 10:06:26 -0500

    sketch out future distributed-array metadata

--- src/DistributedNDVecArray.hxx
+++ src/DistributedNDVecArray.hxx
@@ -1,3 +1,7 @@
 #include "DistributedNDVecArray.hh"
 
-
+// ChunkMetadata:
+// --> start_ind, end_ind
+// --> chunk type: {specified, all_zero}
+// --> offset within file to start reading from
+// --> file path
