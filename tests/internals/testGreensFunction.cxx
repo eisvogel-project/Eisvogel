417559bf57d6bb8f30eee56b741e16ee6b81d192
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-04-12 10:37:18 -0500

    simple cross-check integration routine for unit test

--- tests/internals/testGreensFunction.cxx
+++ tests/internals/testGreensFunction.cxx
@@ -1,3 +1,4 @@
+#include <chrono>
 #include "GreensFunction.hh"
 #include "Symmetry.hh"
 #include "Eisvogel/IteratorUtils.hh"
@@ -19,14 +20,14 @@ void fill_array(NDVecArray<T, dims, vec_dims>& to_fill, const Vector<std::size_t
   IteratorUtils::index_loop_over_array_elements(to_fill, worker);
 }
 
-// evaluates a linear function
+// evaluates a toy Green's function in (t, r, z) coordinates
 template <std::size_t dims, std::size_t vec_dims>
-Vector<scalar_t, vec_dims> linear(const Vector<scalar_t, dims>& pos, const Vector<scalar_t, dims>& coeffs) {
+Vector<scalar_t, vec_dims> toy_green(const Vector<scalar_t, dims>& pos, const Vector<scalar_t, dims>& coeffs) {
 
   scalar_t lin_val = VectorUtils::inner(pos, coeffs);  
   Vector<scalar_t, vec_dims> retvec;
   for(std::size_t i = 0; i < vec_dims; i++) {
-    retvec[i] = (i + 1) * lin_val;
+    retvec[i] = (i + 1) * lin_val * pos[0] * pos[0] / 100.0;
   }
   
   return retvec;
@@ -48,6 +49,46 @@ void register_chunks(DistributedNDVecArray<NDVecArray, T, dims, vec_dims>& darr,
   };
   
   IteratorUtils::index_loop_over_chunks(start_ind, end_ind, chunk_shape, chunk_registerer);  
+}
+
+template <class CallableT>
+scalar_t integrate_simple(scalar_t t_sig, const LineCurrentSegment& seg, CallableT&& green) {
+
+  auto get_F_cart = [](const RZFieldVector& field, const XYZCoordVector& pos) -> XYZFieldVector {
+    scalar_t x = pos.x();
+    scalar_t y = pos.y();
+    scalar_t r_xy = std::sqrt(pos.x() * pos.x() + pos.y() * pos.y());
+
+    scalar_t cos_phi = 0.0, sin_phi = 0.0;
+    if(r_xy > 0) {
+      cos_phi = x / r_xy;
+      sin_phi = y / r_xy;
+    }
+    
+    return XYZFieldVector{
+      field.r() * cos_phi,
+      field.r() * sin_phi,
+      field.z()
+    };
+  };
+  
+  scalar_t stepsize = 0.01f;
+  
+  XYZCoordVector seg_vel = (seg.end_pos - seg.start_pos) / (seg.end_time - seg.start_time);
+  XYZCoordVector source_xyz = seg_vel * seg.charge;
+
+  scalar_t sigval = 0.0;
+  for(scalar_t t_p = seg.start_time; t_p < seg.end_time; t_p += stepsize) {
+    XYZCoordVector cur_pos = seg.start_pos + seg_vel * (t_p - seg.start_time);
+    XYZFieldVector green_val = get_F_cart(green(t_sig - t_p, cur_pos), cur_pos);
+
+    for(std::size_t i = 0; i < 3; i++) {
+      sigval += green_val[i] * source_xyz[i];
+    }
+  }
+  sigval *= stepsize;
+  
+  return sigval;
 }
 
 int main(int argc, char* argv[]) {
@@ -76,7 +117,7 @@ int main(int argc, char* argv[]) {
     Vector<std::size_t, dims> end_ind(400);
     
     auto filler = [&](const Vector<std::size_t, dims>& ind){
-      return linear<dims, vec_dims>(ind.template as_type<scalar_t>(), coeffs);
+      return toy_green<dims, vec_dims>(ind.template as_type<scalar_t>(), coeffs);
     };
     register_chunks(darr, start_ind, end_ind, chunk_size, filler);
     
@@ -107,26 +148,26 @@ int main(int argc, char* argv[]) {
   {
     CylindricalGreensFunction gf(workdir, 5);
 
-    std::size_t num_samples = 100;
+    std::size_t num_samples = 1;
     
     std::vector<scalar_t> accum(num_samples, 0.0);
     // RZCoordVector cur_pt{123.0f, 123.0f};
     RZCoordVector cur_pt{28.2843f, 20.0f};
     //scalar_t t_start = 123.0f;
-    scalar_t t_start = 90.0f;
-    scalar_t t_samp = 1.0f;
+    scalar_t t_start = 0.001f;
+    scalar_t t_samp = 0.1;
 
     // RZFieldVector source{1.0f, 1.0f};
     RZFieldVector source{7.07107f, 0.0f};
 
-    gf.accumulate_inner_product<Interpolation::Kernel::Keys>(cur_pt, t_start, t_samp, num_samples,
+    gf.accumulate_inner_product<Interpolation::Kernel::Linear>(cur_pt, t_start, t_samp, num_samples,
 							     source, accum.begin());
 
     // test closure
     std::cout << "Testing closure ... ";
     for(std::size_t sample_ind = 0; sample_ind < num_samples; sample_ind++) {
       RZTCoordVector pos{cur_pt.r(), cur_pt.z(), t_start + t_samp * sample_ind};
-      Vector<scalar_t, vec_dims> field = linear<dims, vec_dims>(pos, coeffs);
+      Vector<scalar_t, vec_dims> field = toy_green<dims, vec_dims>(pos, coeffs);
       
       scalar_t inner_product = 0.0;
       for(std::size_t i = 0; i < vec_dims; i++) {
@@ -134,37 +175,63 @@ int main(int argc, char* argv[]) {
       }
 
       if(std::fabs((inner_product - accum[sample_ind]) / inner_product) > 1e-6) {
+	std::cout << "---------" << std::endl;
 	std::cout << "Problem!" << std::endl;
+	std::cout << "pos = " << pos << std::endl;
+	std::cout << "---------" << std::endl;
       }
     }
     std::cout << "OK!" << std::endl;
   }
-
-  // test some integration against a current
+  
+  // test some integration against a line current
   {
     CylindricalGreensFunction gf(workdir, 5);
 
-    scalar_t t_sig_start = 123.0f;
+    scalar_t t_sig_start = 110.0f;
     scalar_t t_sig_samp = 1.0f;
-    std::size_t num_samples = 100;
+    std::size_t num_samples = 240;
     std::vector<scalar_t> signal_buffer(num_samples);
 
     // build current segment
-    scalar_t track_start_time = 122.0f;
-    scalar_t track_end_time = 124.0f;
+    scalar_t track_start_time = 120.0f;
+    scalar_t track_end_time = 350.0f;
     scalar_t track_charge = 1.0f;
     LineCurrentSegment track(XYZCoordVector{10.0f, 10.0f, 20.0f},    // track start position
-			     XYZCoordVector{20.0f, 20.0f, 20.0f},   // track end position
+			     XYZCoordVector{20.0f, 20.0f, 40.0f},   // track end position
 			     track_start_time, track_end_time, track_charge);
 
+    auto start = std::chrono::high_resolution_clock::now();
+    
     // integrate the current against the Green's function
     gf.apply_accumulate<Interpolation::Kernel::Keys>(track, t_sig_start, t_sig_samp, num_samples, signal_buffer);
 
-    std::cout << "BBBB got final signal BBBB" << std::endl;
-    for(auto cur : signal_buffer) {
-      std::cout << cur << " ";
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);  
+
+    auto green_for_eval = [&](scalar_t t, const XYZCoordVector& pos) {
+
+      if(t < 0) {
+	return Vector2D<scalar_t>{0.0f, 0.0f};
+      }
+      
+      Vector<scalar_t, 3> pos_trz(0);
+      pos_trz[0] = t;
+      pos_trz[1] = std::sqrt(pos.x() * pos.x() + pos.y() * pos.y());
+      pos_trz[2] = pos.z();
+      
+      return toy_green<3, 2>(pos_trz, coeffs);
+    };
+    
+    std::cout << "BBBB got final signal BBBB" << std::endl;    
+    for(std::size_t i = 0; i < signal_buffer.size(); i++) {
+
+      scalar_t exp_sig = integrate_simple(t_sig_start + i * t_sig_samp, track, green_for_eval);
+      
+      std::cout << "t = " << t_sig_start + i * t_sig_samp << " , signal = " << signal_buffer[i] << ", expected = " << exp_sig << " deviation = " << (signal_buffer[i]-exp_sig)/exp_sig << std::endl;
     }
     std::cout << std::endl;
+    std::cout << "took " << duration << std::endl;
   }
   
   std::cout << "done" << std::endl;  
