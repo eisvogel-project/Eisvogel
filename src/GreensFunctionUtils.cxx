c847a7e5fb05c0a5572b0298c2ac42373f2914c4
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-04-15 09:24:29 -0500

    current status. will not compile yet

--- src/GreensFunctionUtils.cxx
+++ src/GreensFunctionUtils.cxx
@@ -1,10 +1,11 @@
 #include "Eisvogel/GreensFunctionUtils.hh"
+#include "Eisvogel/IteratorUtils.hh"
 #include "DistributedNDVecArray.hh"
 #include "GreensFunction.hh"
 
 namespace GreensFunctionUtils {
 
-  void CreateElectricDipoleGreensFunction(std::filesystem::path gf_path, const RZTCoordVector& start_coords, const RZTCoordVector& end_coords, scalar_t ior,
+  void CreateElectricDipoleGreensFunction(std::filesystem::path gf_path, const RZCoordVector& start_coords, const RZCoordVector& end_coords, scalar_t t_end, scalar_t ior,
 					  scalar_t filter_t_peak, unsigned int filter_order, scalar_t r_min,
 					  scalar_t os_factor, std::size_t max_pts_in_chunk) {
     
@@ -17,12 +18,37 @@ namespace GreensFunctionUtils {
     scalar_t lambda_min = c / (fmax * ior);
     scalar_t delta_t = 1.0 / (2 * fmax * os_factor);
     scalar_t delta_pos = lambda_min / (2.0 * os_factor);
+
+    scalar_t t_start = 0.0;
     
     std::cout << "---------------------------" << std::endl;
     std::cout << "Using oversampling factor = " << os_factor << std::endl;
     std::cout << "delta_t = " << delta_t << std::endl;
     std::cout << "delta_r = delta_z = " << delta_pos << std::endl;
-    std::cout << "start_coords: t = " << start_coords.t() << ", r = " << start_coords.r() << ", z = " << start_coords.z() << std::endl;
-    std::cout << "---------------------------" << std::endl;    
+    std::cout << "start_coords: t = " << t_start << ", r = " << start_coords.r() << ", z = " << start_coords.z() << std::endl;
+    std::cout << "end_coords: t = " << t_end << ", r = " << end_coords.r() << ", z = " << end_coords.z() << std::endl;
+    std::cout << "---------------------------" << std::endl;
+
+    RZTVector<scalar_t> stepsize_requested{delta_pos, delta_pos, delta_t};
+    RZTCoordVector start_coords_rzt;
+    RZTCoordVector end_coords_rzt;
+    
+    auto fill_and_register_chunk = [](const RZTIndexVector& chunk_start, const RZTIndexVector& chunk_end){
+
+      
+    };
+
+    RZTIndexVector start_inds(0);
+    RZTIndexVector end_inds = ((end_coords_rzt - start_coords_rzt) / stepsize_requested).template as_type<std::size_t>();
+    RZTVector<std::size_t> chunk_size(max_pts_in_chunk);
+    IteratorUtils::index_loop_over_chunks(start_inds, end_inds, chunk_size);
+    
+    // DeltaVector domain_size = end_coords - start_coords;
+    // CoordVector number_pts = domain_size / stepsize_requested;
+
+    // IndexVector number_chunks = number_pts / max_pts_in_chunk + 1;
+    // DeltaVector chunk_size = domain_size / (DeltaVector)(number_chunks);
+    // IndexVector number_pts_in_chunk = chunk_size / stepsize_requested + 1;    
+
   }
 }
