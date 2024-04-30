cc707f014d0c2dc79c3c855a57452b4fa47fef49
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2024-04-29 21:43:42 -0500

    add field scale factor

--- examples/plot_greens_function/greens_function_absmax_annotated.py
+++ examples/plot_greens_function/greens_function_absmax_annotated.py
@@ -29,6 +29,7 @@ def greens_function_absmax_annotated(exported_green_path, outpath, config_path, 
     number_annotations = len(annotations)
 
     data = np.load(exported_green_path) # data comes in [r, z, t, field_dim], where field_dim = {E_r, E_z}
+    data *= float(config["field_scale_fact"])
     E_r_data = data[:,:,:,0]
     E_z_data = data[:,:,:,1]
     E_abs_data = np.linalg.norm(data, axis = 3)
@@ -48,12 +49,12 @@ def greens_function_absmax_annotated(exported_green_path, outpath, config_path, 
             
     # Field plot
     ax_field = fig.add_subplot(gs[1])
-    fieldplot = plotting_utils.plot_field(ax_field, E_abs_max_data, range_x = config["range_x"], range_y = config["range_y"], epilog = field_annotation_epilog)
+    fieldplot = plotting_utils.plot_field(ax_field, E_abs_max_data, range_x = config["range_x"], range_y = config["range_y"], epilog = field_annotation_epilog, fs = fs)
 
     divider = make_axes_locatable(ax_field)
     cax = divider.append_axes("right", size="5%", pad=0.05)
     cbar = fig.colorbar(fieldplot, cax = cax)
-    cbar.set_label("$K_\mathrm{max}$ [a.u.]", fontsize = fs)
+    cbar.set_label("$|\mathbf{K}|_\mathrm{max}$ [a.u.]", fontsize = fs)
     cbar.ax.tick_params(labelsize = fs)
 
     # Annotations
@@ -61,11 +62,12 @@ def greens_function_absmax_annotated(exported_green_path, outpath, config_path, 
     for ind, (cur_annotation, cur_title) in enumerate(zip(annotations, annotation_labels)):
         ax_annotation = fig.add_subplot(gs_detail[ind])
         cur_detail_pos_ind = pos_to_index(cur_annotation["pos"], E_abs_max_data.shape, config["range_x"], config["range_y"])
-        ax_annotation.plot(tvals, E_z_data[int(cur_detail_pos_ind[0]), int(cur_detail_pos_ind[1]), :], color = "black", label = r"$E_z$")
-        ax_annotation.plot(tvals, E_r_data[int(cur_detail_pos_ind[0]), int(cur_detail_pos_ind[1]), :], color = "gray", label = r"$E_r$")
+        ax_annotation.plot(tvals, E_z_data[int(cur_detail_pos_ind[0]), int(cur_detail_pos_ind[1]), :], color = "black", label = r"$K_z$")
+        ax_annotation.plot(tvals, E_r_data[int(cur_detail_pos_ind[0]), int(cur_detail_pos_ind[1]), :], color = "gray", label = r"$K_r$")
         ax_annotation.set_xlim(*cur_annotation["range_t"])
         ax_annotation.set_xlabel("Time [ns]", fontsize = fs)
-        ax_annotation.set_title(cur_title, fontsize = fs)
+        # ax_annotation.set_title(cur_title, fontsize = fs)
+        ax_annotation.text(cur_title, fontsize = fs)
         ax_annotation.tick_params(axis = "y", direction = "in", left = True, right = True, labelsize = fs)
         ax_annotation.tick_params(axis = "x", direction = "in", bottom = True, top = True, labelsize = fs)    
         ax_annotation.legend(frameon = False)
