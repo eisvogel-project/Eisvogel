75280939b12b962f055c5e56e707d0bfe73d86a3
Philipp Windischhofer <philipp.windischhofer@gmail.com>
2024-05-31 17:58:41 -0500

    More Green's function plots (#36)
    Plots to visualize Green's functions:
    
    * Animation exported as gif (can be converted to mp4 later if desired)
    * Static plot of max. amplitude
    * Static plot of max. amplitude together with annotations of the time-domain behavior at selected locations

--- examples/plot_greens_function/animate_greens_function.py
+++ examples/plot_greens_function/animate_greens_function.py
@@ -1,28 +1,31 @@
-import argparse
+import argparse, yaml, os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.colors as colors
 
-def generate_animation(outpath, data, range_x, range_y, range_t, xlabel = "r [m]", ylabel = "z [m]", zlabel = "", epilog = None, fs = 13):
+def generate_animation(outpath, data, range_x, range_y, range_t, xlabel = "r [m]", ylabel = "z [m]", zlabel = "", epilog = None, fs = 13, downsample_t = 1):
     num_frames = data.shape[2]
     vals_x = np.linspace(range_x[0], range_x[1], data.shape[0])
     vals_y = np.linspace(range_y[0], range_y[1], data.shape[1])
 
     vals_mesh_x, vals_mesh_y = np.meshgrid(vals_x, vals_y)
 
-    fig = plt.figure(figsize = (6, 5), dpi = 200)
+    figsize_y = 5
+    figsize_x = (range_x[1] - range_x[0]) / (range_y[1] - range_y[0]) * figsize_y * 1.35
+    
+    fig = plt.figure(figsize = (figsize_x, figsize_y), dpi = 400)
     ax = fig.add_subplot(111)
 
     fig.subplots_adjust(left = 0.15, right = 0.8, top = 0.95, bottom = 0.1)
 
-    linthresh = 1e-10
-    vmin = -2e-5
-    vmax = 2e-5
+    linthresh = 1e-8
+    vmin = -2e-3
+    vmax = 2e-3
 
     fieldplot = ax.pcolormesh(vals_mesh_x, vals_mesh_y, np.transpose(data[:, :, 0]), 
-                            cmap = "bwr", shading = "nearest",
+                            cmap = "bwr", shading = "gouraud",
                             norm = colors.SymLogNorm(linthresh = linthresh, linscale = 1, base = 10, vmin = vmin, vmax = vmax))
     ax.set_aspect("equal")
     ax.set_xlabel(xlabel, fontsize = fs)
@@ -41,23 +44,31 @@ def generate_animation(outpath, data, range_x, range_y, range_t, xlabel = "r [m]
     cbar.ax.tick_params(labelsize = fs)
 
     if epilog:
-        epilog(ax)
-
+        epilog(ax)        
+        
     def animate(it):
-        fieldplot.set_array(np.transpose(data[:, :, it]).ravel())
-        timestep_text.set_text(r"$t = {:.2f}\,\mathrm{{ns}}$".format(range_t[0] + it * (range_t[1] - range_t[0]) / num_frames))
+        ind_t = downsample_t * it
+        fieldplot.set_array(np.transpose(data[:, :, ind_t]).ravel())
+        timestep_text.set_text(r"$t = {:.2f}\,\mathrm{{ns}}$".format(range_t[0] + ind_t * (range_t[1] - range_t[0]) / num_frames))
+        print(f"Rendered frame {ind_t} / {num_frames}")
         return fieldplot
     
-    anim = animation.FuncAnimation(fig, animate, frames = num_frames, interval = 50, repeat = False)
+    anim = animation.FuncAnimation(fig, animate, frames = int(num_frames / downsample_t), interval = 5, repeat = False)
 
     writer = animation.PillowWriter(fps = 15,
-                                metadata = dict(artist='Me'),
-                                bitrate = 1800)
+                                    metadata = dict(artist='Me'),
+                                    bitrate = 3000)
     anim.save(outpath, writer = writer)
 
     plt.close()
 
-def animate_greens_function(exported_green_path, outpath):
+def animate_greens_function(exported_green_path, outdir, config_path):
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    
+    with open(config_path) as configfile:
+        config = yaml.safe_load(configfile)
     
     data = np.load(exported_green_path)
     
@@ -68,14 +79,20 @@ def animate_greens_function(exported_green_path, outpath):
     def show_interface(ax):
         ax.axhline(0.0, color = "gray", ls = "dashed")
 
-    generate_animation("E_z.gif", E_z_data, range_x = (0, 400.0 / 3.0), range_y = (-300.0 / 3.0, 200.0 / 3.0), range_t = (0.0, 500.0),
+    outpath = os.path.join(outdir, "E_z.gif")
+    generate_animation(outpath, E_z_data, range_x = config["range_x"], range_y = config["range_y"], range_t = config["range_t"],
                        zlabel = r"$E_z$ [a.u.]", epilog = show_interface)
+
+    outpath = os.path.join(outdir, "E_r.gif")
+    generate_animation(outpath, E_r_data, range_x = config["range_x"], range_y = config["range_y"], range_t = config["range_t"],
+                       zlabel = r"$E_r$ [a.u.]", epilog = show_interface)
     
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--green_exported", action = "store", dest = "exported_green_path")
-    parser.add_argument("--out", action = "store", dest = "outpath")
+    parser.add_argument("--outdir", action = "store", dest = "outdir")
+    parser.add_argument("--config", action = "store", dest = "config_path")
     args = vars(parser.parse_args())
 
     animate_greens_function(**args)
