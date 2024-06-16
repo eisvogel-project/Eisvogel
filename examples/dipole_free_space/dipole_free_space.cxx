d6a9e0a38bd444ca9ba025d2077d2c8c68687c78
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-06-16 15:02:08 -0500

    settings for homogeneous ice green's function

--- examples/dipole_free_space/dipole_free_space.cxx
+++ examples/dipole_free_space/dipole_free_space.cxx
@@ -10,19 +10,19 @@ int main(int argc, char* argv[]) {
   std::string gf_path = argv[1];
 
   // Domain of weighting field
-  RZCoordVector start_coords{0.0f, -100.0f};
+  RZCoordVector start_coords{0.0f, -200.0f};
   RZCoordVector end_coords{300.0f, 100.0f};
-  scalar_t t_end = 300;
+  scalar_t t_end = 400;
 
   // Index of refraction
-  scalar_t ior = 1.0;
+  scalar_t ior = 1.42;
   
   // Filter parameters
-  scalar_t filter_t_peak = 5.0;   // lowpass filter peaking time
+  scalar_t filter_t_peak = 3.0;   // lowpass filter peaking time
   unsigned int filter_order = 4;  // lowpass filter order
 
   // Sampling parameters
-  scalar_t os_factor = 20;
+  scalar_t os_factor = 15;
   scalar_t r_min = 0.1;
 
   GreensFunctionCalculator::Analytic::ElectricDipole(gf_path, start_coords, end_coords, t_end, ior, filter_t_peak, filter_order, r_min, os_factor);
