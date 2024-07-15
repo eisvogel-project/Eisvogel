053cc33bb8d7e70811e127672a9d384839a56790
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-07-15 09:15:16 -0500

    revert formatting on Current.hh

--- eisvogel/core/Current.hh
+++ eisvogel/core/Current.hh
@@ -3,15 +3,14 @@
 #include "Vector.hh"
 
 struct LineCurrentSegment {
-
-  LineCurrentSegment(XYZCoordVector&& start_pos, XYZCoordVector&& end_pos,
-                     scalar_t start_time, scalar_t end_time, scalar_t charge);
+  
+  LineCurrentSegment(XYZCoordVector&& start_pos, XYZCoordVector&& end_pos, scalar_t start_time, scalar_t end_time, scalar_t charge);
 
   XYZCoordVector start_pos;
   XYZCoordVector end_pos;
   scalar_t start_time;
   scalar_t end_time;
-  scalar_t charge;
+  scalar_t charge;  
 };
 
 #include "Current.hxx"
