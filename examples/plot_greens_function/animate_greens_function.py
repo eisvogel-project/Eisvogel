91501b8d50a692acbd8353c628cad545517d4ab0
Philipp Windischhofer <windischhofer@uchicago.edu>
2024-11-06 09:13:18 -0600

    much faster animations

--- examples/plot_greens_function/animate_greens_function.py
+++ examples/plot_greens_function/animate_greens_function.py
@@ -1,6 +1,9 @@
 import argparse, yaml, os
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 import matplotlib.animation as animation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.colors as colors
@@ -8,27 +11,20 @@ import imageio_ffmpeg as ffmpeg
 
 def generate_animation(outpath, data, range_x, range_y, range_t, xlabel = "r [m]", ylabel = "z [m]", zlabel = "", epilog = None, fs = 13, downsample_t = 1):
     num_frames = data.shape[2]
-    vals_x = np.linspace(range_x[0], range_x[1], data.shape[0])
-    vals_y = np.linspace(range_y[0], range_y[1], data.shape[1])
 
-    vals_mesh_x, vals_mesh_y = np.meshgrid(vals_x, vals_y)
-
-    fig_scale = 0.4
     figsize_y = 5
-    figsize_x = (range_x[1] - range_x[0]) / (range_y[1] - range_y[0]) * figsize_y * fig_scale
+    figsize_x = (range_x[1] - range_x[0]) / (range_y[1] - range_y[0]) * figsize_y + figsize_y * 0.33
     
-    fig = plt.figure(figsize = (figsize_x, figsize_y), dpi = 400)
-    ax = fig.add_subplot(111)
+    fig = plt.figure(figsize = (figsize_x, figsize_y), dpi = 300, layout = "constrained")
+    gs = GridSpec(1, 1, figure = fig)
+    ax = fig.add_subplot(gs[0])
 
-    fig.subplots_adjust(left = 0.15, right = 0.8, top = 0.95, bottom = 0.1)
-
-    linthresh = 1e-8
+    linthresh = 1e-5
     vmin = -2e-3
     vmax = 2e-3
 
-    fieldplot = ax.pcolormesh(vals_mesh_x, vals_mesh_y, np.transpose(data[:, :, 0]), 
-                            cmap = "bwr", shading = "gouraud",
-                            norm = colors.SymLogNorm(linthresh = linthresh, linscale = 1, base = 10, vmin = vmin, vmax = vmax))
+    fieldplot = ax.pcolorfast(range_x, range_y, np.transpose(data[:, :, 0]), cmap = "bwr",
+                              norm = colors.SymLogNorm(linthresh = linthresh, linscale = 1, base = 10, vmin = vmin, vmax = vmax))
     ax.set_aspect("equal")
     ax.set_xlabel(xlabel, fontsize = fs)
     ax.set_ylabel(ylabel, fontsize = fs)
@@ -50,7 +46,7 @@ def generate_animation(outpath, data, range_x, range_y, range_t, xlabel = "r [m]
         
     def animate(it):
         ind_t = downsample_t * it
-        fieldplot.set_array(np.transpose(data[:, :, ind_t]).ravel())
+        fieldplot.set_array(np.transpose(data[:, :, ind_t]))
         timestep_text.set_text(r"$t = {:.2f}\,\mathrm{{ns}}$".format(range_t[0] + ind_t * (range_t[1] - range_t[0]) / num_frames))
         print(f"Rendered frame {ind_t} / {num_frames}")
         return fieldplot
@@ -65,7 +61,6 @@ def generate_animation(outpath, data, range_x, range_y, range_t, xlabel = "r [m]
     anim.save(outpath, writer = writer)
 
     plt.close()
-
 
 def animate_greens_function(exported_green_path, outdir, config_path):
 
