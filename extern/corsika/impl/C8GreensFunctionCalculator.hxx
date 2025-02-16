5282f08cce86d339407f8c06d41dd11b005e2432
Philipp Windischhofer <philipp.windischhofer@gmail.com>
2025-02-16 13:33:57 -0600

    propagate initialization

--- extern/corsika/impl/C8GreensFunctionCalculator.hxx
+++ extern/corsika/impl/C8GreensFunctionCalculator.hxx
@@ -1,13 +1,21 @@
 #include "C8GreensFunctionCalculator.hh"
 #include "MEEPCylindricalGreensFunctionCalculator.hh"
 
-C8CylindricalGreensFunctionCalculator::C8CylindricalGreensFunctionCalculator(float r_max, float z_min, float z_max, float z_ant,
-									     std::function<eps_signature> eps,
-									     std::function<impulse_response_signature> impulse_response) {
-
+C8CylindricalGreensFunctionCalculator::C8CylindricalGreensFunctionCalculator(int argc, char* argv[]) {
+  m_mpi = std::make_unique<meep::initialize>(argc, argv);
 }
 
-void C8CylindricalGreensFunctionCalculator::calculate(std::filesystem::path gf_path,
+C8CylindricalGreensFunctionCalculator::~C8CylindricalGreensFunctionCalculator() = default;
+
+void C8CylindricalGreensFunctionCalculator::calculate(float r_max, float z_min, float z_max, float z_ant, float t_end,
+						      std::function<eps_signature> eps,
+						      std::function<impulse_response_signature> impulse_response,
+						      std::filesystem::path gf_path,
 						      std::filesystem::path local_scratchdir, std::filesystem::path global_scratchdir) {
 
+  m_geom = std::make_unique<CylinderGeometry>(r_max, z_min, z_max, eps);
+  m_ant = std::make_unique<InfEDipoleAntenna>(0.0, t_end, z_ant, impulse_response);
+  m_gfc = std::make_unique<GreensFunctionCalculator::MEEP::CylindricalGreensFunctionCalculator>(*m_geom, *m_ant, t_end);
+  
+  m_gfc -> Calculate(gf_path, local_scratchdir, global_scratchdir);
 }
--- extern/corsika/include/C8GreensFunctionCalculator.hh
+++ extern/corsika/include/C8GreensFunctionCalculator.hh
@@ -3,25 +3,36 @@
 #include <functional>
 #include <filesystem>
 
+// Forward declarations
 namespace GreensFunctionCalculator::MEEP {
   class CylindricalGreensFunctionCalculator;
+}
+class CylinderGeometry;
+class Antenna;
+namespace meep {
+  class initialize;
 }
 
 class C8CylindricalGreensFunctionCalculator {
 
 public:
+  
+  C8CylindricalGreensFunctionCalculator(int argc, char* argv[]);
+  ~C8CylindricalGreensFunctionCalculator();
 
   using eps_signature = float(float, float); // (r, z) -> eps
   using impulse_response_signature = float(float); // t -> current
   
-  C8CylindricalGreensFunctionCalculator(float r_max, float z_min, float z_max, float z_ant,
-					std::function<eps_signature> eps,
-					std::function<impulse_response_signature> impulse_response);
-
-  void calculate(std::filesystem::path gf_path, std::filesystem::path local_scratchdir, std::filesystem::path global_scratchdir);
+  void calculate(float r_max, float z_min, float z_max, float z_ant, float t_end,
+		 std::function<eps_signature> eps,
+		 std::function<impulse_response_signature> impulse_response,
+		 std::filesystem::path gf_path, std::filesystem::path local_scratchdir, std::filesystem::path global_scratchdir);
   
 private:
 
+  std::unique_ptr<meep::initialize> m_mpi;
   std::unique_ptr<GreensFunctionCalculator::MEEP::CylindricalGreensFunctionCalculator> m_gfc;
+  std::unique_ptr<CylinderGeometry> m_geom;
+  std::unique_ptr<Antenna> m_ant;
 
 };
