a6582ba6e6622469f7c0cc0d4683750855e3bd52
Philipp Windischhofer <philipp.windischhofer@gmail.com>
2025-02-16 17:18:51 -0600

    Green's function calculation from C8 (#68)
    Provides infrastructure for C8 to call into Eisvogel for Green's function calculations.

--- extern/corsika/impl/C8SignalCalculator.hxx
+++ extern/corsika/impl/C8SignalCalculator.hxx
@@ -7,8 +7,10 @@
 
 C8SignalCalculator::C8SignalCalculator(std::filesystem::path gf_path, std::size_t cache_depth) {
   
-  m_gf = std::make_shared<CylindricalGreensFunction>(gf_path, cache_depth);
+  m_gf = std::make_unique<CylindricalGreensFunction>(gf_path, cache_depth);
 }
+
+C8SignalCalculator::~C8SignalCalculator() = default;
 
 void C8SignalCalculator::get_antenna_location(std::array<float, 3>& ant_xyz) {
   // TODO: store in Green's function metadata and read back
