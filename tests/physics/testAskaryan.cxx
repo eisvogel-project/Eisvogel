e47f89127ebe62e27e53bdb98891c2e18370bd4e
Philipp Windischhofer <philipp.windischhofer@gmail.com>
2026-06-05 10:21:59 -0500

    use local scratchdir

--- tests/physics/testAskaryan.cxx
+++ tests/physics/testAskaryan.cxx
@@ -20,7 +20,7 @@ void create_greens_function(std::filesystem::path outdir, scalar_t filter_t_peak
   scalar_t os_factor = 20;
   scalar_t r_min = 0.1;
   
-  std::filesystem::path scratchdir = getenv("TMPDIR");
+  std::filesystem::path scratchdir = "./tmp";
   GreensFunctionCalculator::Analytic::ElectricDipole(outdir, scratchdir, start_coords, end_coords, t_end, ior, filter_t_peak, filter_order, r_min, os_factor);
 }
 
